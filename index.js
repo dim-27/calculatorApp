e4555193d5973eaebc9128d809419792285e8f47
dim-27 <dimasfirmanda27@gmail.com>
2023-08-14 22:58:27 +0700

    1. [x] ganti posisi pembagian jadi 4 perkalian jadi 3 2. [x] tambah pembagian

---
+++ index.js
@@ -0,0 +1,49 @@
+var sumIt = 1;
+var subtractIt = 2;
+var multiplyIt = 3;
+var divideIt = 4;
+var square = 5;
+var squareRoot = 6;
+var squareRoot3 = 7;
+
+var operatorOpt = prompt(`Select operator : 
+    1.Penjumlahan
+    2.Pengurangan
+    3.Perkalian
+    4.Pembagian
+    5.Pangkat 2
+    6.Akar 2
+    7.Akar 3
+`);
+
+function showResult(whatResult) {
+  console.log(whatResult);
+  document.write(whatResult);
+  document.write("<br>");
+}
+
+var doSomething = function (num1, num2) {
+  if (operatorOpt == sumIt) {
+    // Kak Gusti Silahkan diisi
+  } else if (operatorOpt == subtractIt) {
+    showResult("The result is: " + (num1 - num2));
+  } else if (operatorOpt == multiplyIt) {
+    // Kak raymond Silahkan diisi
+  } else if (operatorOpt == divideIt) {
+    // Kak dimas Silahkan diisi
+    showResult("The result is: " + (num1 / num2))
+  } else if (operatorOpt == square) {
+    // Kak Evelio Silahkan diisi
+  } else if (operatorOpt) {
+    // Kak Rais Silahkan disi
+  } else if (operatorOpt == squareRoot3) {
+    showResult("The result is: " + Math.cbrt(num1));
+  } else {
+    console.log("Tidak Menerima Input");
+  }
+};
+
+doSomething(
+  parseInt(prompt("Enter first number: ")),
+  parseInt(prompt("Enter second number: "))
+);
